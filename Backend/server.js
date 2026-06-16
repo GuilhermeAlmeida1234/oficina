c3e59c56d182cd68de348fb3dbb70135b6c0323d
GUILHERME ALMEIDA DOS SANTOS <guilherme.asantos126@senacsp.edu.br>
2026-06-16 15:37:26 -0300

    att

--- Backend/server.js
+++ Backend/server.js
@@ -12,3 +12,30 @@ app.use("/usuarios", oficinaRoutes);
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });
+
+const conexao = require('./database/conexao');
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.log('Erro ao conectar ao banco MySQL do Aiven:');
+        console.log(erro.message);
+        return
+    }
+
+    console.log('Conexão realizada com sucesso!');
+    console.log('Conectado ao banco MySQL do Aiven.');
+
+    conexao.query('SELECT NOW () AS data_hora_atual', (erro, resultados))
+        if (erro) {
+            console.log('Erro ao executar consulta:');
+            console.log(erro.message);
+            conexao.end();
+            return;
+        }
+
+        console.log('Consulta executada com sucesso!');
+        console.log('Data e hora retornada pelo banco:');
+        console.log(resultados[0].data_hora_atual);
+
+        conexao.end();
+})
